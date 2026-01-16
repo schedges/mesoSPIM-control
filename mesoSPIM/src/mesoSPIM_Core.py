Fixed some bugs in the z-stp and f-step calculation, updated display bar on acq window, fixed bug in writer

mesoSPIM/src/mesoSPIM_Core.py
@@ -53,6 +53,8 @@ class mesoSPIM_Core(QtCore.QObject):
     sig_add_images_to_image_series = QtCore.pyqtSignal(Acquisition, AcquisitionList)
     sig_end_image_series = QtCore.pyqtSignal(Acquisition, AcquisitionList)
     sig_write_metadata = QtCore.pyqtSignal(Acquisition, AcquisitionList)
+    sig_acq_status = QtCore.pyqtSignal(str)
+
     sig_prepare_live = QtCore.pyqtSignal()
     sig_get_live_image = QtCore.pyqtSignal()
     sig_get_snap_image = QtCore.pyqtSignal(bool)
@@ -116,6 +118,7 @@ class mesoSPIM_Core(QtCore.QObject):
         self.camera_worker.sig_overheat_stop.connect(self.stop)
         self.sig_end_image_series.connect(self.camera_worker.end_image_series, type=QtCore.Qt.BlockingQueuedConnection)
         self.sig_stop_aquisition.connect(self.camera_worker.stop, type=QtCore.Qt.QueuedConnection)
+        self.sig_acq_status.connect(self.parent.acquisition_manager_window.AcqStatusLabel.setText)
         
         self.image_writer_thread = QtCore.QThread()
         self.image_writer = mesoSPIM_ImageWriter(self, self.frame_queue, self.timestamp_queue)
@@ -796,17 +799,26 @@ class mesoSPIM_Core(QtCore.QObject):
         self.sig_state_request.emit({'etl_l_amplitude' : acq['etl_l_amplitude']})
         self.sig_state_request.emit({'etl_l_offset' : acq['etl_l_offset']})
         self.f_steps,self.z_steps = acq.get_f_and_z_steps()
-        self.abs_f_positions,self.abs_z_positions = acq.get_f_and_z_move_positions(self.f_steps,self.z_steps)
 
         if self.TTL_mode_enabled_in_cfg is True:
             first_nonzero_z = self.z_steps[np.flatnonzero(self.z_steps)[0]] if len(np.flatnonzero(self.z_steps)) > 0 else 0
             first_nonzero_f = self.f_steps[np.flatnonzero(self.f_steps)[0]] if len(np.flatnonzero(self.f_steps)) > 0 else 0
-            self.move_relative({"f_rel":-1*first_nonzero_f,"z_rel":-1*first_nonzero_z})
+            
+            self.move_relative({"x_rel":0,"y_rel":0,"z_rel":-1*first_nonzero_z,"f_rel":-1*first_nonzero_f,"theta_rel":0})
             time.sleep(0.1)
-            self.move_relative({"f_rel":first_nonzero_f,"z_rel":first_nonzero_z})
+            self.move_relative({"x_rel":0,"y_rel":0,"z_rel":first_nonzero_z,"f_rel":first_nonzero_f,"theta_rel":0})
             time.sleep(0.1)
             self.sig_state_request.emit({'ttl_movement_enabled_during_acq': True})
             time.sleep(0.05)
+
+        #Get absolute positions for tracking/writing
+        self.serial_worker.stage.report_position()
+        abs_pos_start = self.serial_worker.stage.state['position_absolute']
+        self.abs_z_positions = [abs_pos_start['z_pos']]
+        self.abs_f_positions = [abs_pos_start['f_pos']]
+        for i in range(0,len(self.f_steps)-1):
+            self.abs_z_positions.append(self.abs_z_positions[-1]+self.z_steps[i])
+            self.abs_f_positions.append(self.abs_f_positions[-1]+self.f_steps[i])
 
         self.sig_status_message.emit('Preparing camera: Allocating memory')
         self.sig_prepare_image_series.emit(acq, acq_list) # signal to the Camera
@@ -825,6 +837,7 @@ class mesoSPIM_Core(QtCore.QObject):
         laser = self.state['laser']
         self.laserenabler.enable(laser)
         laser_blanking = False if (hasattr(self.cfg, 'laser_blanking') and (self.cfg.laser_blanking in ('stack', 'stacks'))) else True
+        
         for i in range(steps):
             if self.stopflag is True:
                 self.close_image_series()
@@ -839,9 +852,14 @@ class mesoSPIM_Core(QtCore.QObject):
                 self.snap_image_in_series(laser_blanking)
                 self.sig_add_images_to_image_series.emit(acq, acq_list)
 
+                #Update current parameters
+                f_abs = self.abs_f_positions[i] if i < len(self.abs_f_positions) else None
+                z_abs = self.abs_z_positions[i] if i < len(self.abs_z_positions) else None
+
+                self.sig_acq_status.emit(f"Displayed Slice {i+1}/{steps} | Z: {z_abs}  F: {f_abs}")
+                            
                 ''' Get the current correct f_step and z step '''
-                move_dict = {"f_rel":self.f_steps[i],
-                             "z_rel":self.z_steps[i]}
+                move_dict = {"x_rel":0,"y_rel":0,"z_rel":self.z_steps[i],"f_rel":self.f_steps[i],"theta_rel":0}
                 logger.debug(f'move_dict: {move_dict}')
                 self.move_relative(move_dict)
 

mesoSPIM/src/utils/acquisitions.py
@@ -198,8 +198,6 @@ class Acquisition(indexed.IndexedOrderedDict):
         f_positions = np.round(f_positions,5)
         #Calculate deltas
         f_steps = np.diff(f_positions)
-        #Following the last image, do nothing
-        f_steps = np.append(f_steps,0.)
 
         #Calculate Z steps
         if self["z_end"] > self["z_start"]:
@@ -207,48 +205,11 @@ class Acquisition(indexed.IndexedOrderedDict):
         else:
             z_steps = np.array([-1*abs(self["z_step"]) for i in range(0,nSteps)])
 
+        #Following the last image, do nothing
+        f_steps = np.append(f_steps,0.)
+        z_steps[-1]=0
+
         return f_steps,z_steps
-    
-    def get_f_and_z_move_positions(self,f_steps,z_steps):
-        startpoint = self.get_startpoint()
-        z_positions = startpoint["z_abs"] + np.cumsum(np.concatenate(([0.],z_steps[:-1])))
-        f_positions = startpoint["f_abs"] + np.cumsum(np.concatenate(([0.],f_steps[:-1])))
-        return f_positions,z_positions
-        
-    def get_focus_stepsize_generator(self, f_stage_min_step_um=0.25):
-        ''''
-        Provides a generator object to correct rounding errors for focus tracking acquisitions.
-
-        The focus stage has to travel a shorter distance than the sample z-stage, ideally only
-        a fraction of the z-step size. However, due to the limited minimum step size of the focus stage,
-        rounding errors can accumulate over thousands of steps.
-
-        Therefore, the generator tracks the rounding error and applies correcting steps here and there
-        to minimize the error.
-
-        This assumes a minimum step size of around 0.25 micron that the focus stage is capable of.
-
-        This method contains lots of round functions to keep residual rounding errors at bay.
-        '''
-        steps = self.get_image_count()
-        f_step = abs((self['f_end'] - self['f_start'])/steps)
-        logger.debug(f"Focus interpolation: f_start, f_end, f_step, steps: {self['f_start'], self['f_end'], f_step, steps}")
-        feasible_f_step = max(f_stage_min_step_um * (f_step // f_stage_min_step_um),
-                              f_stage_min_step_um)  # Round to nearest multiple of f_stage_min_step_um
-        if self['f_end'] < self['f_start']:
-            f_step = -f_step
-            feasible_f_step = -feasible_f_step
-
-        expected_focus = 0
-        focus = 0
-        for i in range(steps):
-            focus_error = round(expected_focus - focus, 5)
-            new_step = round(focus_error / f_stage_min_step_um) * f_stage_min_step_um + feasible_f_step # this can be zero, and it is correct
-            yield new_step
-            logger.debug(f"Relative focus: new_step, actual, expected, error: {new_step, focus, expected_focus, focus_error}, um")
-            focus += new_step
-            focus = round(focus, 5)
-            expected_focus += f_step
 
 
 class AcquisitionList(list):